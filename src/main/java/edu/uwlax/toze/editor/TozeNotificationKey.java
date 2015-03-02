530b048b060913f4900398bd9f31bc2c47c37a14
dstoleson <dstoleson@mac.com>
2015-03-02 09:15:55 -0600

    Move enums into own definitions.

---
+++ src/main/java/edu/uwlax/toze/editor/TozeNotificationKey.java
@@ -0,0 +1,14 @@
+package edu.uwlax.toze.editor;
+
+/**
+* Created by dhs on 3/1/15.
+*/
+public enum TozeNotificationKey
+{
+    KEY_NOTIFICATION_TYPE,
+    KEY_CLASSDEF,
+    KEY_OPERATION,
+    KEY_SPECIFICATION_DOCUMENT,
+    KEY_OBJECT_INDEX,
+    KEY_ERRORS
+}
---
+++ src/main/java/edu/uwlax/toze/editor/TozeNotificationType.java
@@ -0,0 +1,16 @@
+package edu.uwlax.toze.editor;
+
+/**
+* Created by dhs on 3/1/15.
+*/
+public enum TozeNotificationType
+{
+    CLASS_ADDED,
+    CLASS_REMOVED,
+    CLASS_RENAMED,
+    OPERATION_ADDED,
+    OPERATION_REMOVED,
+    OPERATION_RENAMED,
+    SPECIFICATION_RENAMED,
+    ERRORS
+}
